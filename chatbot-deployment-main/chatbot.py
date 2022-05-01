626956e70293898843d8ec3ce59f716add196e67
Betulguse <amoghrew@gmail.com>
2022-05-01 09:56:09 +0530

    Chatbot and Integration Updates

--- chatbot-deployment-main/chatbot.py
+++ chatbot-deployment-main/chatbot.py
@@ -1,3 +1,5 @@
+from distutils.log import ERROR
+from lib2to3.pytree import _Results
 import random 
 import json
 import pickle
@@ -30,32 +32,14 @@ def bag_of_words(sentence):
                 bag[i]=1
     return np.array(bag)
 
-def predict_class(sentence):
+def predic_class(sentence):
     bow = bag_of_words(sentence)
-    res = model.predict(np.array([bow]))[0]
+    res = model.predict(np.array([bow])[0])
     ERROR_THRESHOLD = 0.25
     results = [[i,r] for i , r in enumerate(res) if r>ERROR_THRESHOLD]
 
     results.sort(key= lambda x : x[1], reverse=True)
     return_list = []
+    
 
-    for r in results:
-        return_list.append({'intents': classes[r[0]], 'probability': str(r[1])})
-    return return_list
 
-def get_response(intents_list, intents_json):
-    tag = intents_list[0]['intents']
-    list_of_intents = intents_json['intents']
-    for i in list_of_intents:
-        if i['tag'] == tag:
-            result = random.choice(i['responses'])
-            break
-    return result
-
-print("Yay. Bot Works")
-
-while True:
-    message = input("")
-    ints = predict_class(message)
-    res = get_response(ints, intents)
-    print(res)
